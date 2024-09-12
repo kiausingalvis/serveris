ddd23b2bf5e14d953bc2fea4e1c562cccb4c3d94
vdle <markask20@gmail.com>
2024-09-12 19:09:05 +0100

    a

--- src/main/java/com/example/examplemod/events/AnvilRenameHandler.java
+++ src/main/java/com/example/examplemod/events/AnvilRenameHandler.java
@@ -1,5 +1,7 @@
 package com.example.examplemod.events;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraftforge.event.AnvilUpdateEvent;
@@ -11,21 +13,6 @@ public class AnvilRenameHandler {
 
     @SubscribeEvent
     public static void onAnvilUpdate(AnvilUpdateEvent event) {
-        ItemStack leftSlot = event.getLeft();
-        ItemStack rightSlot = event.getRight();
-
-        if (isCustomItem(leftSlot)) {
-            if (!event.getName().isEmpty()) {
-                event.setCanceled(true);
-            }
-        }
-    }
-
-    private static boolean isCustomItem(ItemStack itemStack) {
-        if (itemStack.hasTag()) {
-            CompoundTag tag = itemStack.getTag();
-            return tag != null && tag.contains("UniqueId");
-        }
-        return false;
+        event.setCanceled(true);
     }
 }
